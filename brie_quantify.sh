ee62bc7ad11e045eea4ea81e01c78e9335d65406
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-06-27 12:11:44 +0100

    Delete brie_quantify.sh

--- brie_quantify.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source Simulation/venv/bin/activate
-brie -a Simulation/brie_splice_events/SE.gold.gtf -s Simulation/bamfiles/simulated/ERR1211610XSAligned.sortedByCoord.out.bam -f mouse_features.csv.gz -o out_dir -p 15
