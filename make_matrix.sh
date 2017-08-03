90646584eba5b368f5ef0c65854f0936973a8ab5
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-08-03 14:49:43 +0100

    Update make_matrix.sh
    Add Sailfish to make_matrix.sh

--- make_matrix.sh
+++ make_matrix.sh
@@ -5,7 +5,7 @@ make_matrix() {
   #Make a data matrix of results for the tools just run. Salmon requires special treatment as it was run on three different modes, putting results in three different directories
   if [ "$1" == "Salmon" ]; then
     python ./generate.py Salmon_align `pwd` Simulation/Salmon_results/Salmon_Alignment_Results
-    python ./generate.py Salmon_SMEM `pwd` Simulation/Salmon_results/Salmon_SMEM_results
+    python ./generate.py Salmon_SMEM `pwd` Simulation/Salmon_results/Salmon_SMEM_results    
     python ./generate.py Salmon_quasi `pwd` Simulation/Salmon_results/Salmon_quasi_results
     chmod +x Salmon_align_TPM.sh
     chmod +x Salmon_SMEM_TPM.sh
@@ -20,7 +20,19 @@ make_matrix() {
     #Clean up to save space
     #rm -r Simulation/$1"_results"
 
-  elif [ "$1" == "Kallisto" ] || [ "$1" == "eXpress" ] || [ "$1" == "RSEM" ]; then
+  elif [ "$1" == "ground_truth" ]; then
+    python ./generate.py $1 `pwd` Simulation/data/simulated
+    chmod +x $1"_TPM.sh"
+    chmod +x $1"_FPKM.sh"
+    chmod +x $1"_Counts.sh"
+    ./$1"_TPM.sh"
+    ./$1"_FPKM.sh"
+    ./$1"_Counts.sh"
+    rm $1"_TPM.sh"
+    rm $1"_FPKM.sh"
+    rm $1"_Counts.sh"
+  
+  elif [ "$1" == "Kallisto" ] || [ "$1" == "eXpress" ] || [ "$1" == "RSEM" ] || [ "$1" == "Sailfish" ]; then
     echo $1
     python ./generate.py $1 `pwd` Simulation/$1"_results"
     echo python ./generate.py $1 `pwd` Simulation/$1"_results"
@@ -51,3 +63,4 @@ make_matrix() {
 }
 
 "$@"
+
