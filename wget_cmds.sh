c603ea2a53b107a20749e04428e494a68e767e00
jenni-westoby <jenni-westoby@users.noreply.github.com>
2017-11-09 17:02:28 +0000

    Delete wget_cmds.sh

--- wget_cmds.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#cd into directory you want to put files in
-cd /lustre/scratch108/compgen/team218/jw28/kolod
-
-#download files into directory
-cat /lustre/scratch108/compgen/team218/jw28/weblinks.txt | /lustre/scratch108/compgen/team218/jw28/parallel --gnu "wget -t 100 {}"
